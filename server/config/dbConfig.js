f7b49fa3922045feb6137d749d178ff9f2ba6b5d
chandra76768 <121227250+chandra76768@users.noreply.github.com>
2024-07-07 16:20:37 +0530

    Update dbConfig.js

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/fit_Pak");
+    await mongoose.connect("mongodb+srv://chandrareddycheruku76768:<password>@cluster0.ap6xzui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/fit_Pak");
 
     console.log("Connected to MongoDB");
   } catch (error) {
